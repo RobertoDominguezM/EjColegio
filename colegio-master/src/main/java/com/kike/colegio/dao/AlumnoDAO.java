aa251f5b29d77697b95b26469b301da6b049ed46
RobertoDominguezM <66738153+RobertoDominguezM@users.noreply.github.com>
2020-11-26 12:31:09 +0100

    Add files via upload

---
+++ colegio-master/src/main/java/com/kike/colegio/dao/AlumnoDAO.java
@@ -0,0 +1,13 @@
+package com.kike.colegio.dao;
+
+import java.util.List;
+
+import com.kike.colegio.dtos.Alumno;
+
+public interface AlumnoDAO {
+	
+	List<Alumno> obtenerTodosAlumnos();
+	List<Alumno> obtenerAlumnosporIdyNombre(String id, String nombre);
+	Integer insertarAlumno(String id, String nombre, String idMunicipio);
+
+}
